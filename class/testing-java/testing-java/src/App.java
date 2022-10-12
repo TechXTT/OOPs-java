247d3d1f5fce33e541dc4f3e494d3af0873e8246
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2022-10-12 23:41:50 +0300

    [CLASS] Java Hello World

--- class/testing-java/testing-java/src/App.java
+++ class/testing-java/testing-java/src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Integer a = 1;
+        Long b = 2L;
+        System.out.println(a + b);
+        Float c = 3.0f;
+        Double d = 4.0d;
+        Boolean e = true;
+        System.out.printf("%f, %f, %b\n", c, d, e);
+        Character f = 'f';
+        String g = "g";
+        System.out.println(f + g);
     }
 }
